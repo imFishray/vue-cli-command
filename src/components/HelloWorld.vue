7bd5f83233568f3bb8b8bddb35690157496e0bc3
imFishray <yulielove0530@gmail.com>
2022-11-19 15:38:34 +0800

    123

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="hello">
+    <div class="img">
+      <img src="" alt="">
+    </div>
+    <div>
+      <p>{{ title }}</p>
+      <p>{{ msg }}</p>
+      <button>搶票喔</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    title: String,
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+.hello{
+  width: 100%;
+  display: inline-flex;
+  margin: 1rem;
+}
+.img{
+  width: 8rem;
+  height: 8rem;
+  background-color: #ddd;
+}
+</style>
---
+++ src/views/AboutView.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <Button type="primary">Primary</Button>
+  </div>
+  <Carousel v-model="value" loop>
+        <CarouselItem v-for="item in 5" :key="item">
+            <div class="demo-carousel">
+              <img :src="require('@/assets/logo.png')" alt="" />
+            </div>
+        </CarouselItem>
+        <!-- <CarouselItem>
+            <div class="demo-carousel">1</div>
+        </CarouselItem>
+        <CarouselItem>
+            <div class="demo-carousel">2</div>
+        </CarouselItem>
+        <CarouselItem>
+            <div class="demo-carousel">3</div>
+        </CarouselItem>
+        <CarouselItem>
+            <div class="demo-carousel">4</div>
+        </CarouselItem> -->
+  </Carousel>
+</template>
+<script>
+    export default {
+        data () {
+            return {
+                value: 0
+            }
+        }
+    }
+</script>
